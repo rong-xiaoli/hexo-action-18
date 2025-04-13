8b4316837c4123b144987bca8f3b751ee4ccf8a7
容小狸 <58361774+rong-xiaoli@users.noreply.github.com>
2025-04-14 04:46:51 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,20 +3,25 @@
 set -e
 
 # setup ssh-private-key
+echo "Setting up ssh private key... "
 mkdir -p /root/.ssh/
 echo "$INPUT_DEPLOY_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+echo "Done. "
 
 # setup deploy git account
+echo "Configuring git account. "
 git config --global user.name "$INPUT_USER_NAME"
 git config --global user.email "$INPUT_USER_EMAIL"
 
 # install hexo env
+echo "Installing hexo. "
 npm install hexo-cli -g
 npm install hexo-deployer-git --save
 
 # deployment
+echo "Deploying. "
 if [ "$INPUT_COMMIT_MSG" = "none" ]
 then
     hexo g -d
